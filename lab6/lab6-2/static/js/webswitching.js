225f6861eaa73ce93496b8a18d25b6a08468e28a
Filas312 <filas312@gmail.com>
2021-01-22 10:36:46 +0100

    Added final version

--- lab6/lab6-2/static/js/webswitching.js
+++ lab6/lab6-2/static/js/webswitching.js
@@ -1,42 +1,38 @@
 $(document).ready(function() {
-  var sites = ["https://wp.pl","https://tvp.info"];
-  var switch_probability = 0.67;
-    
-  var wp = 0;
-  var tvp = 0;
+  var sites = ["https://wp.pl","https://trojmiasto.pl","https://zakopane.pl"];
   
-  var dice = Math.random();
-  var site = "";
+  var sendDate = (new Date()).getTime();
+
+  var img = new Image();
+
+  var times = {};
+  var min_time = 9999;
+  var min_site = "";
   
-  if(typeof sessionStorage.wp === 'undefined') {
-	sessionStorage.wp = 0;
-  } else {
-	wp = parseInt(sessionStorage.wp);
+  var fun = function(site) { 
+  
+    var img = new Image(); 
+	
+	var start = new Date().getTime();
+	
+	var timer = function() { 
+	  var dur = new Date().getTime() - start; 
+	  if(dur < min_time) {
+		min_time = dur;
+		min_site = site; 
+		console.log(`Min_site is ${site} with ${dur}`);
+	  }
+	  console.log( `Site: ${site}: ${dur}ms` ); 
+	};
+	
+	img.onload  = timer;
+	img.onerror = timer;	
+	
+	return img.src = site + "/favicon.ico"; 
   }
   
-  if(typeof sessionStorage.tvp === 'undefined') {
-    sessionStorage.tvp = 0;
-  } else {
-	tvp = parseInt(sessionStorage.tvp);
-  }
-  
-  if(dice > switch_probability) {
-    sessionStorage.tvp = tvp + 1;
-	tvp = parseInt(sessionStorage.tvp);
-	site = sites[1];
-  } else {
-	sessionStorage.wp = wp + 1;
-	wp = parseInt(sessionStorage.wp);
-	site = sites[0];
-  }
-  
-  if(tvp+wp != 0) {
-	alert(`Wybrano: ${site}\nPrób: ${tvp+wp}, WP: ${((wp/(tvp+wp))*100).toFixed(2)}%, TVP: ${((tvp/(tvp+wp))*100).toFixed(2)}%`);
-  }
-  else {
-    alert(`Wybrano: ${site}\nPrób: ${tvp+wp}, WP: 0%, TVP: 0%`);
-  }
-  
-  window.location.replace(site);  
-  
+  sites.forEach(fun);  
+ 
+ 
+  window.setTimeout(() => {window.location.replace(min_site);}, 1500);
 });
